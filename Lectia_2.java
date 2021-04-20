ffa8e9bd92b8c95f0bd98603b5409c4898d0a3fd
GitHubbDima <NinfaLight@mail.ru>
2021-04-20 19:36:34 +0300

    init added 1 App.class 1 Lectia_2.class, Readme.txt, 1 folder Configurations with Config.txt
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
    
     On branch master
    
     Initial commit
    
     Changes to be committed:
    	new file:   App.java
    	new file:   Configuration/Config.txt
    	new file:   Lectia_2.java
    	new file:   Readme.txt

---
+++ Lectia_2.java
@@ -0,0 +1,10 @@
+public class Lectia_2{
+	
+	public static void main(String[] args){
+		int a = 2, b;
+		b = 5;
+		int sum = a + b;
+		System.out.println("Rezultatul este: " + sum);
+		System.out.println($"Rezultatul este: {sum}");
+	}
+}
