c5d9227e967bbf520d19709d7a1d8e67e8dc9762
GitHubbDima <NinfaLight@mail.ru>
2021-04-22 19:11:46 +0300

    Operatii cu doua variabile

---
+++ Ab.java
@@ -0,0 +1,11 @@
+public class Ab{
+	public static void main(String[] args){
+		int a = 32;
+		int b = 23;
+		
+		System.out.println("suma: " + (a + b));
+		System.out.println("suma: " + (a - b));
+		System.out.println("suma: " + ((double)a / b));
+		System.out.println("suma: " + (a * b));
+	}
+}
